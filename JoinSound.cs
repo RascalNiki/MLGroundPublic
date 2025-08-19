0bb1687ed1e838391bd0c84a341ff4720e126fe0
RascalNiki <seichi.rascal97@gmail.com>
2025-08-19 13:31:36 +0900

    Add files via upload

---
+++ JoinSound.cs
@@ -0,0 +1,16 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+public class JoinSound : UdonSharpBehaviour
+{
+    [SerializeField]
+    private AudioSource audioSource;
+
+    public void OnPlayerJoined()
+    {
+        if (audioSource != null)
+        {
+            audioSource.Play();
+        }
+    }
+}
