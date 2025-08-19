a91ca5db718e0a2b7f61012de316c3620232035b
RascalNiki <seichi.rascal97@gmail.com>
2025-08-19 12:54:39 +0900

    Add files via upload

---
+++ Leaderboard.cs
@@ -0,0 +1,33 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Image;
+using VRC.SDKBase;
+using VRC.Udon.Common.Interfaces;
+
+public class Leaderboard : UdonSharpBehaviour
+{
+    public VRCUrl imageUrl;
+    public new Renderer renderer;
+    private VRCImageDownloader _imageDownloader;
+    private IUdonEventReceiver _udonEventReceiver;
+
+    void Start()
+    {
+        DownloadImage();
+    }
+
+    public override void Interact()
+    {
+        DownloadImage();
+    }
+
+    private void DownloadImage()
+    {
+        _imageDownloader = new VRCImageDownloader();
+        _udonEventReceiver = (IUdonEventReceiver)this;
+
+        var rgbInfo = new TextureInfo();
+        rgbInfo.GenerateMipMaps = true;
+        _imageDownloader.DownloadImage(imageUrl, renderer.material, _udonEventReceiver, rgbInfo);
+    }
+}
