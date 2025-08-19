e2e708826e353f5b2b7dd93087d5d604e3ec5140
RascalNiki <seichi.rascal97@gmail.com>
2025-08-19 13:34:16 +0900

    Add files via upload

---
+++ ShowTableTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+public class ShowTableTrigger : UdonSharpBehaviour
+{
+    [SerializeField]
+    private GameObject table;
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        if (player.isLocal)
+        {
+            table.SetActive(true);
+        }
+    }
+
+    /*public override void OnPlayerTriggerExit(VRCPlayerApi player)
+    {
+        if (player.isLocal)
+        {
+            table.SetActive(false);
+        }
+    }*/
+}
